f11e8f4736e017a8f036c02593f206dfced1b678
Tareq1131 <tareqislam076@gmail.com>
2024-04-25 05:16:35 +0600

    Fetching Data with useQuery

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+
+import HomePage from "./components/HomePage";
+import { Link } from "react-router-dom";
+import { SuperHeroesPage } from "./components/SuperHeroesPage";
+import RQSuperHeroespage from "./components/RQSuperHeroesPage";
+import {QueryClientProvider, QueryClient} from 'react-query'
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/super-heroes">Traditional Super Heroes</Link>
+            </li>
+            <li>
+              <Link to="/rq-super-heroes">RQ Super Heroes</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/super-heroes" element={<SuperHeroesPage />} />
+          <Route path="/rq-super-heroes" element={<RQSuperHeroespage />} />
+        </Routes>
+      </div>
+    </Router>
+    </QueryClientProvider>
   );
 }
 
